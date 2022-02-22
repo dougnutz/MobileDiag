f62696c8a4b7d7a69738dceb24ce89e47272a1bf
Doug L <drl.ecm.tech@gmail.com>
2022-02-22 12:40:26 -0500

    add price list in code, add two column flex layout, basics working, Fixed health page

--- Pages/health.cshtml.cs
+++ Pages/health.cshtml.cs
@@ -1,1 +1,19 @@
-ok
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MobileDiag.Pages;
+
+public class HealthModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public HealthModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
